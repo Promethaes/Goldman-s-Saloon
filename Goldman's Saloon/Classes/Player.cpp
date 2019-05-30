ee4d6c13d6023c077f71643db20e7d2cb3baeff5
Anthony <anth19800@hotmail.com>
2019-05-30 03:07:16 -0400

    dqwdqwdqw

--- Goldman's Saloon/Classes/Player.cpp
+++ Goldman's Saloon/Classes/Player.cpp
@@ -2,6 +2,7 @@
 #include "GameObject.h"
 #include "Primitive.h"
 #include <iostream>
+#define DEADZONE 0.2f
 namespace Sedna {
 	Player::Player(XinputController* CONTROLLER, const char * path,cocos2d::Scene* scene, const cocos2d::Vec2 & LOCATION, float RADIUS)
 		:GameObject(path,scene,LOCATION,RADIUS)
@@ -17,16 +18,16 @@ namespace Sedna {
 	void Player::checkInput()
 	{
 		pController->updateSticks(pSticks);
-		if (pSticks[0].x > -0.3f && pSticks[0].x < 0.3f && pSticks[0].y > -0.3f && pSticks[0].y < 0.3f)
-			hitbox->addForce(hitbox->getVelocity().x *-3.0f, hitbox->getVelocity().y*-3.0f);
+		if (pSticks[0].x > -DEADZONE && pSticks[0].x < DEADZONE && pSticks[0].y > -DEADZONE && pSticks[0].y < DEADZONE)
+			hitbox->addForce(hitbox->getVelocity().x *-10.0f, hitbox->getVelocity().y*-10.0f);
 		else {
 			auto direction = cocos2d::Vec2(pSticks[0].x, pSticks[0].y);
 			auto force = direction / sqrt(direction.x*direction.x + direction.y*direction.y);//normalized vector
 
-			hitbox->setForce(cocos2d::Vec2((pSticks[0].x > 0.3f || pSticks[0].x < -0.3f) ?
-				force.x * 300 : 0.0f,
-				(pSticks[0].y > 0.3f || pSticks[0].y < -0.3f) ?
-				force.y * 300 : 0.0f));
+			hitbox->setForce(cocos2d::Vec2((pSticks[0].x > DEADZONE || pSticks[0].x < -DEADZONE) ?
+				force.x * 250 : 0.0f,
+				(pSticks[0].y > DEADZONE || pSticks[0].y < -DEADZONE) ?
+				force.y * 250 : 0.0f));
 		}
 	}
 }
